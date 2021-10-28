708fc9a91156970c4f65332f14401435c313a016
reivosar <users.noreply.github.com>
2021-10-29 02:59:57 +0900

    メソッド移動

--- common/src/main/java/reivosar/common/util/log/LoggersType.java
+++ common/src/main/java/reivosar/common/util/log/LoggersType.java
@@ -16,4 +16,13 @@ enum LoggersType
 	abstract Loggers getLoggers(String name);
 
 	abstract Loggers getLoggers(Class<?> clazz);
+	
+
+	static Loggers getDefaultLoggers(String name) {
+		return LOGBACK.getLoggers(name);
+	}
+
+	static Loggers getDefaultLoggers(Class<?> clazz) {
+		return LOGBACK.getLoggers(clazz);
+	}
 }
