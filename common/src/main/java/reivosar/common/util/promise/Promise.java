839c92abfdee5eae6c85d4f85d2463eedfb9ad28
reivosar <users.noreply.github.com>
2022-08-23 00:28:04 +0900

    refactoring

--- common/src/main/java/reivosar/common/util/promise/Promise.java
+++ common/src/main/java/reivosar/common/util/promise/Promise.java
@@ -10,107 +10,106 @@ import reivosar.common.util.model.Result;
 /**
  * The Promise object represents the result of the completion (or failure)
  * of an asynchronous operation and the value of the result.
- * 
+ * <p>
  * Instructions are below.
  * <pre>
  *     Promise.resolve(() -> getAccessToken())
- *         .then (token -> ...) 
+ *         .then (token -> ...)
  *         .onSuccess (System.out::println);
- * </pre> 
- *  
+ * </pre>
+ *
  * @param <T> The type of result that this Promise holds
  */
-public interface Promise<T> extends Result<T>
-{
-	/**
-	 * Asynchronously processes the behavior of the 
-	 * Supplier passed as an argument. 
-	 * 
-	 * It waits for a certain period of time until 
-	 * the processing is completed, and returns a 
-	 * Promise object holding the result when it is completed.
-	 * 
-	 * @param <T> The type of result that this Promise holds
-	 * @param supplier supplier to generate Promise
-	 * @return {@link Promise}
-	 */
-	static <T>Promise<T> resolve(Supplier<T> supplier) {
-        return new PromiseHandler<T>().resolve(supplier).await();
-	}
-
+public interface Promise<T> extends Result<T> {
     /**
      * Asynchronously processes the behavior of the
      * Supplier passed as an argument.
-     *
+     * <p>
      * It waits for a certain period of time until
      * the processing is completed, and returns a
      * Promise object holding the result when it is completed.
      *
-     * @param <T> The type of result that this Promise holds
+     * @param <T>      The type of result that this Promise holds
      * @param supplier supplier to generate Promise
-     * @param timeout the maximum time to wait
      * @return {@link Promise}
      */
-    static <T>Promise<T> resolve(Supplier<T> supplier, long timeout) {
+    static <T> Promise<T> resolve(Supplier<T> supplier) {
+        return new PromiseHandler<T>().resolve(supplier).await();
+    }
+    
+    /**
+     * Asynchronously processes the behavior of the
+     * Supplier passed as an argument.
+     * <p>
+     * It waits for a certain period of time until
+     * the processing is completed, and returns a
+     * Promise object holding the result when it is completed.
+     *
+     * @param <T>      The type of result that this Promise holds
+     * @param supplier supplier to generate Promise
+     * @param timeout  the maximum time to wait
+     * @return {@link Promise}
+     */
+    static <T> Promise<T> resolve(Supplier<T> supplier, long timeout) {
         return new PromiseHandler<T>(PromiseConfig.builder().timeout(timeout).build())
                 .resolve(supplier).await();
     }
-
+    
     /**
      * Generate a new Promise based on the results processed by
      * the previous Promise. If an exception has been thrown
      * before, this method will be skipped.
      *
-     * @param <R> The type of result that this Promise holds
+     * @param <R>      The type of result that this Promise holds
      * @param function function to receive and handle the result
      * @return {@link Promise}
      */
     <R> Promise<R> then(Function<? super T, ? super R> function);
-
+    
     /**
      * Generate a new Promise based on the results processed by
      * the previous Promise. If an exception has been thrown
      * before, this method will be skipped.
      *
-     * @param <R> The type of result that this Promise holds
+     * @param <R>      The type of result that this Promise holds
      * @param function function to receive and handle the result
-     * @param timeout the maximum time to wait
+     * @param timeout  the maximum time to wait
      * @return {@link Promise}
      */
     <R> Promise<R> then(Function<? super T, ? super R> function, long timeout);
-
-	/**
-	 * If the series of processing is successful, the result of 
-	 * the processing is handled with the behavior of the Consumer 
-	 * object passed as an argument.
-	 * 
-	 * If the process fails, this method will not be called.
-	 * 
-	 * @param consumer consumer to receive and process the results of the previous promise
-	 * @return {@link Promise}
-	 */
-	Promise<T> onSuccess(final Consumer<T> consumer);
-
-	/**
-	 * If an exception was occured in the previous process, 
-	 * Exception object is handled with the behavior of the Consumer 
-	 * object passed as an argument. 
-	 * 
-	 * If the processing succeeds, this method will not be called.
-	 * 
-	 * @param consumer consumer to receive and process the results of the previous promise
-	 * @return {@link Promise}
-	 */
-	Promise<T> onFailure(final Consumer<Throwable> consumer);
-	
-	/**
-	 * An exception will be thrown if the previous process 
-	 * threw an exception.
-	 * 
-	 * @return {@link Promise}
-	 * @throws PromiseException　This exception will be thrown if an error occurs during promise generation.
-	 */
-	Promise<T> ifErrorPresentThrow() throws PromiseException;
+    
+    /**
+     * If the series of processing is successful, the result of
+     * the processing is handled with the behavior of the Consumer
+     * object passed as an argument.
+     * <p>
+     * If the process fails, this method will not be called.
+     *
+     * @param consumer consumer to receive and process the results of the previous promise
+     * @return {@link Promise}
+     */
+    Promise<T> onSuccess(final Consumer<T> consumer);
+    
+    /**
+     * If an exception was occured in the previous process,
+     * Exception object is handled with the behavior of the Consumer
+     * object passed as an argument.
+     * <p>
+     * If the processing succeeds, this method will not be called.
+     *
+     * @param consumer consumer to receive and process the results of the previous promise
+     * @return {@link Promise}
+     */
+    Promise<T> onFailure(final Consumer<Throwable> consumer);
+    
+    /**
+     * An exception will be thrown if the previous process
+     * threw an exception.
+     *
+     * @return {@link Promise}
+     * @throws PromiseException 　This exception will be thrown if an error occurs during promise generation.
+     */
+    Promise<T> ifErrorPresentThrow() throws PromiseException;
     
     /**
      * Methods for batch execution of multiple suppliers.
