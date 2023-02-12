d03810173d1f83ad60a21b888108b9b4c8603b22
reivosar <users.noreply.github.com>
2023-02-13 00:01:27 +0900

    refactored test code

--- common/src/test/java/reivosar/common/util/io/pdf/creator/PdfCreatorTest.java
+++ common/src/test/java/reivosar/common/util/io/pdf/creator/PdfCreatorTest.java
@@ -1,23 +1,32 @@
 package reivosar.common.util.io.pdf.creator;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PdfCreatorTest {
     
-    private static final Path OUTPUT_PDF_PATH = Paths.get(System.getProperty("user.dir"),
-            "src\\test\\resources\\templates\\pdf\\dest", "test.pdf");
+    private static final String THIS_TESTCASE_WORK_DIR = "\\src\\test\\resources\\templates\\pdf\\dest";
+    private static final Path OUTPUT_PDF_DIR = Paths.get(System.getProperty("user.dir"), THIS_TESTCASE_WORK_DIR);
+    
+    @BeforeEach
+    void setup() throws IOException {
+        deleteDirectory();
+        Files.createDirectories(OUTPUT_PDF_DIR);
+    }
     
     @Test
     void shouldBeCreatedNewPDF() {
         // GIVEN
+        final Path outputPdfFilePath = OUTPUT_PDF_DIR.resolve("test.pdf");
         // WHEN
         final boolean result = PdfCreator.forCreatingNew()
                 .append(new EmbedTextParameter.Builder()
@@ -28,15 +37,19 @@ class PdfCreatorTest {
                         .align("center")
                         .text(1234567890)
                         .build())
-                .createTo(OUTPUT_PDF_PATH);
+                .createTo(outputPdfFilePath);
         // THEN
         assertTrue(result);
-        assertTrue(Files.exists(OUTPUT_PDF_PATH));
+        assertTrue(Files.exists(outputPdfFilePath));
     }
     
     @AfterEach
-    void removeFile() throws IOException {
-        Files.deleteIfExists(OUTPUT_PDF_PATH);
+    void deleteDirectory() {
+        try (final Stream<Path> list = Files.list(OUTPUT_PDF_DIR)) {
+            list.forEach(path -> path.toFile().deleteOnExit());
+            Files.deleteIfExists(OUTPUT_PDF_DIR);
+        } catch (IOException ignored) {
+            // Do nothing
+        }
     }
-    
 }
