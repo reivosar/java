87d522439bbf4268aaad4799d8da2c728abe2575
reivosar <users.noreply.github.com>
2024-03-12 04:54:12 +0900

    changed font settings

--- src/main/java/reivosar/common/util/io/pdf/PdfBoxStandardFontLoader.java
+++ src/main/java/reivosar/common/util/io/pdf/PdfBoxStandardFontLoader.java
@@ -1,12 +1,14 @@
 package reivosar.common.util.io.pdf;
 
 import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 
 class PdfBoxStandardFontLoader extends FontLoader {
     private final PDFont pdFont;
     
-    PdfBoxStandardFontLoader(final PDFont pdFont) {
-        this.pdFont = pdFont;
+    PdfBoxStandardFontLoader(final String fontName) {
+        this.pdFont = new PDType1Font(Standard14Fonts.getMappedFontName(fontName));
     }
     
     @Override
